0ea3cbbb308cf7dcdb26f61acd1768fe640b5665
adilryk <adilshafiqryk007@gmail.com>
2024-09-27 21:46:13 +0500

    final code

--- get_pricee.py
+++ get_pricee.py
@@ -1,6 +1,24 @@
-def get_price(self, quantity):
-      return quantity * self.price
+def get_price(self,quantity):
+    try:
+        if quantity <=0:
+            raise  ValueError("Quantity must be greater than 0")
+        discount=0
+        if quantity<10:
+           pass
+        elif 10<=quantity<99:
+           discount =10 #10%
+        else:
+           discount=20 #20#
+    #total price
+        price=(100-discount)/100*self.price
+        return price*quantity
 
-def  get_total (self,quantity):
-      return quantity * self.price 
+    except ValueError as e:
+     print(f"Error occured: {e}")
+    return None
+
+
+
+  
+ 
 
